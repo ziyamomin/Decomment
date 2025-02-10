623db1756a9551ac3e98fe56a4125083313faa5a
Ziya Momin <ziyamomin@gmail.com>
2025-02-10 11:52:57 -0500

    Fixed erorrs and made line number a pointer instead of a global variable

--- decomment.c
+++ decomment.c
@@ -1,169 +1,155 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
-enum Statetype {START, IN_COMMENT, ASTERISK, STRING_LITERAL, CHAR_LITERAL, ESCAPE, FORWARD_SLASH, REJECT};
 
-int line_number = 1;
+enum Statetype {
+    START,
+    IN_COMMENT,
+    ASTERISK,
+    STRING_LITERAL,
+    CHAR_LITERAL,
+    ESCAPE,
+    FORWARD_SLASH,
+    REJECT
+};
 
 void print(char c) {
     putchar(c);
 }
 
-void report_error() {
+void report_error(int line_number) {
     fprintf(stderr, "Error: line %d: unterminated comment\n", line_number);
 }
 
-enum Statetype
-handleStartState(int c)
-{
-    enum Statetype state;
-    if(c == '/') {
+enum Statetype handleStartState(int c, int *line_number) {
+    enum Statetype state = START;
+
+    if (c == '/') {
         state = FORWARD_SLASH;
-    } 
-    if(c == '"') {
-        print('"');
+    } else if (c == '"') {
+        print(c);
         state = STRING_LITERAL;
-    }
-    if(c == '\'') {
-        print('\'');
+    } else if (c == '\'') {
+        print(c);
         state = CHAR_LITERAL;
-    }
-    if(c == '\n') {
-        print('\n');
-        state = START;
-        line_number++;
+    } else if (c == '\n') {
+        print(c);
+        (*line_number)++;
     } else {
         print(c);
-        state = START;
     }
     return state;
 }
 
-enum Statetype
-handleForwardSlashState(int c)
-{
-    enum Statetype state;
-    if(c == '*') {
+enum Statetype handleForwardSlashState(int c) {
+    enum Statetype state = START;
+
+    if (c == '*') {
         print(' ');
         state = IN_COMMENT;
     } else {
         print('/');
         print(c);
-        state = START;
     }
     return state;
 }
 
-enum Statetype
-handleInCommentState(int c) {
-    enum Statetype state;
-    if(c == '*') {
+enum Statetype handleInCommentState(int c, int line_number) {
+    enum Statetype state = IN_COMMENT;
+
+    if (c == '*') {
         state = ASTERISK;
-    }
-    if(c == '\n') {
+    } else if (c == '\n') {
         line_number++;
-        state = IN_COMMENT;
-    }
-    if(c == EOF) {
-        report_error();
+    } else if (c == EOF) {
+        report_error(line_number);
         state = REJECT;
-    } else {
-        state = IN_COMMENT;
     }
     return state;
 }
 
-enum Statetype
-handleAsterickCase(int c) {
-    enum Statetype state;
-    if(c == '/') {
+enum Statetype handleAsterickCase(int c) {
+    enum Statetype state = IN_COMMENT;
+    if (c == '/') {
         state = START;
-    }
-    if(c == '*') {
+        // Print space to replace comment
+    } else if (c == '*') {
         state = ASTERISK;
-    } else {
-        state = IN_COMMENT;
     }
+
     return state;
 }
 
-enum Statetype
-handleStringLiteralCase(int c) {
-    enum Statetype state;
-    if(c == '\\') {
-        print('\\');
+enum Statetype handleStringLiteralCase(int c, int *line_number) {
+    enum Statetype state = STRING_LITERAL;
+
+    if (c == '\\') {
+        print(c);
         state = ESCAPE;
-    }
-    if(c == '"') {
-        print('"');
+    } else if (c == '"') {
+        print(c);
         state = START;
-    }
-    if(c == '\n') {
-        print('\n');
-        line_number++;
-        state = STRING_LITERAL;
+    } else if (c == '\n') {
+        print(c);
+        (*line_number)++;
     } else {
         print(c);
-        state = STRING_LITERAL;
     }
+
     return state;
 }
 
-enum Statetype
-handleCharLiteralState(int c) {
-    enum Statetype state;
-    if(c == '\\') {
-        print('\\');
+enum Statetype handleCharLiteralState(int c, int *line_number) {
+    enum Statetype state = CHAR_LITERAL;
+
+    if (c == '\\') {
+        print(c);
         state = ESCAPE;
-    }
-    if(c == '\'') {
-        print('\'');
+    } else if (c == '\'') {
+        print(c);
         state = START;
-    }
-    if(c == '\n') {
-        print('\n');
-        line_number++;
-        state = CHAR_LITERAL;
+    } else if (c == '\n') {
+        print(c);
+        (*line_number)++;
     } else {
         print(c);
-        state = CHAR_LITERAL;
     }
+
     return state;
 }
 
-enum Statetype
-handleEscapeCaseState(int c, enum Statetype prev_state) {
+enum Statetype handleEscapeCaseState(int c, enum Statetype prev_state) {
     print(c);
     return prev_state;
 }
 
-int main(void)
-{
+int main(void) {
     int c;
     enum Statetype state = START;
     enum Statetype prev_state = START;
+    int line_number = 1;
 
     while ((c = getchar()) != EOF) {
         switch (state) {
             case START:
-                state = handleStartState(c);
+                state = handleStartState(c, &line_number);
                 break;
             case FORWARD_SLASH:
                 state = handleForwardSlashState(c);
                 break;
             case IN_COMMENT:
-                state = handleInCommentState(c);
+                state = handleInCommentState(c, line_number);
                 break;
             case ASTERISK:
                 state = handleAsterickCase(c);
                 break;
             case STRING_LITERAL:
                 prev_state = STRING_LITERAL;
-                state = handleStringLiteralCase(c);
+                state = handleStringLiteralCase(c, &line_number);
                 break;
             case CHAR_LITERAL:
                 prev_state = CHAR_LITERAL;
-                state = handleCharLiteralState(c);
+                state = handleCharLiteralState(c, &line_number);
                 break;
             case ESCAPE:
                 state = handleEscapeCaseState(c, prev_state);
@@ -172,9 +158,11 @@ int main(void)
                 return EXIT_FAILURE;
         }
     }
-    if(state == IN_COMMENT || state == ASTERISK) {
-        report_error();
+
+    if (state == IN_COMMENT || state == ASTERISK) {
+        report_error(line_number);
         return EXIT_FAILURE;
     }
+
     return EXIT_SUCCESS;
 }
